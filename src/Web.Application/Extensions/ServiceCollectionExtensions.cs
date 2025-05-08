8d73d9ec27e77eb60db93db843ff53d33f6205be
aafgani <andrya.afgani@gmail.com>
2025-05-08 15:25:49 +0700

    Implement external login

---
+++ src/Web.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace Web.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAuthentication(this IServiceCollection services, ConfigurationManager config)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login"; // Optional override
+                    options.AccessDeniedPath = "/Account/AccessDenied"; // Optional
+                })
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+               {
+                   config.Bind("AzureEntra", options);
+                   options.ResponseType = OpenIdConnectResponseType.Code;
+                   options.Events ??= new OpenIdConnectEvents();
+                   options.Events.OnTokenValidated += OnTokenValidatedFunc;
+                   options.Events.OnAuthorizationCodeReceived += OnAuthorizationCodeReceivedFunc;
+               });
+
+            return services;
+        }
+
+        private static async Task OnAuthorizationCodeReceivedFunc(AuthorizationCodeReceivedContext context)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+
+        private static async Task OnTokenValidatedFunc(TokenValidatedContext context)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+    }
+}
+    
--- src/Web.Application/Program.cs
+++ src/Web.Application/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.Identity.Web.UI;
+using Web.Application.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+
+builder.Services
+    .AddAuthentication(config);
 
 var app = builder.Build();
 
@@ -11,7 +20,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+}    
 
 //To ensure that any incoming HTTP request will automatically be redirected to HTTPS.
 app.UseHttpsRedirection();
