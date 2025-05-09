28a9cd8828b41732e7ff136218665a35f23e2ce5
aafgani <andrya.afgani@gmail.com>
2025-05-09 14:25:32 +0700

    - Update Repo : handling null return - Add view component for App Version - Enhance error pages - Enhance App.RunAsync()

---
+++ src/Web.Application/Utility/CustomViewComponent/FooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Application.Utility.CustomViewComponent
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var version = Environment.GetEnvironmentVariable("APP_VERSION") ?? "1.0.0"; // Default to 1.0.0 if not set
+            return View("Default", version);
+        }
+    }
+}
